ca601ce509b0ac56a048eab0648ed6e39da4666b
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-05-03 23:30:57 +0200

    tailwind using costumized colors | violet da rocketseat

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <div className="flex gap-2">
-      <button className="bg-violet-500 p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
-      <button className="bg-violet-500 p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
+      <button className="bg-[#8257e6] p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
+      <button className="bg-[#8257e6] p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
     </div>
   )
 }
