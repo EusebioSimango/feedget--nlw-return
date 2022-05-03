7e001d4e52acfe4d99769b3d3c3215ff23a30ed0
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-04 00:39:29 +0200

    rendering text onClick of widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,10 +1,19 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
 
+	const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+	const toggleWidgetVisibilty = () => {
+		setIsWidgetOpen(!isWidgetOpen)
+	}
+
 	return (
 		<div className="absolute bottom-5 right-5">
-			<button className="bg-brand-500 flex items-center rounded-full px-3 h-12 text-white group"> 
+			{isWidgetOpen && <p>Hello, World!</p>}
+
+			<button onClick={toggleWidgetVisibilty} className="bg-brand-500 flex items-center rounded-full px-3 h-12 text-white group"> 
 				<ChatTeardropDots className="w-6 h-6" /> 
 
 				<span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
