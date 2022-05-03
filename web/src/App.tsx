6071dbc2db0d52ef92c1a7519fdf791c58f20e23
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-03 23:34:29 +0200

    Cleaning to start feedget!

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,9 +4,8 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className="flex gap-2">
-      <button className="bg-[#8257e6] p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
-      <button className="bg-[#8257e6] p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
+    <div>
+    
     </div>
   )
 }
