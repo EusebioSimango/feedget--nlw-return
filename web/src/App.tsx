bb83f43ac076e0d087d75dad728641dda007cf6d
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-05-03 23:24:08 +0200

    tailwind transition-colors

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <div className="flex gap-2">
-      <button className="button">Ola mundo!</button>
-      <button className="button">Ola mundo!</button>
+      <button className="bg-violet-500 p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
+      <button className="bg-violet-500 p-2 rounded hover:bg-violet-700 transition-colors">Ola mundo!</button>
     </div>
   )
 }
