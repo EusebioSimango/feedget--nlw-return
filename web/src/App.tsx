c630862493b1eddb81f01173a184ceb8ee5407dc
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-05-03 23:18:44 +0200

    tailwind syntax @apply

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <div className="flex gap-2">
-      <button className="bg-violet-500 p-2 rounded">Ola mundo!</button>
-      <button className="bg-violet-500 p-2 rounded">Ola mundo!</button>
+      <button className="button">Ola mundo!</button>
+      <button className="button">Ola mundo!</button>
     </div>
   )
 }
