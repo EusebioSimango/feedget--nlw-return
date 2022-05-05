1330c7973647ee0236e23258a07d12c8e4140130
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-06 01:32:01 +0200

    using Popover

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,27 +1,22 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { useState } from 'react'
 import { WidgetForm } from './WidgetForm'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
 
-	const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-	const toggleWidgetVisibilty = () => {
-		setIsWidgetOpen(!isWidgetOpen)
-	}
-
 	return (
-		<div className="absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end">
-			{isWidgetOpen && <WidgetForm />}
+		<Popover className="absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end">
+			<Popover.Panel><WidgetForm /></Popover.Panel>
 
-			<button onClick={toggleWidgetVisibilty} className="bg-brand-500 flex items-center rounded-full px-3 h-12 text-white group"> 
+			<Popover.Button className="bg-brand-500 flex items-center rounded-full px-3 h-12 text-white group"> 
 				<ChatTeardropDots className="w-6 h-6" /> 
 
 				<span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
 					<span className="pl-2"></span>
 					Feedback
 				</span>
-			</button>
-		</div>
+			</Popover.Button>
+		</Popover>
 		)
 }
