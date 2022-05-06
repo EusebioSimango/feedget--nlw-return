9d54c31b20acec46ecceefd9211c9d32d3afcef0
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-06 02:23:04 +0200

    finalize Button

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,3 +1,33 @@
+import { CloseButton } from './CloseButton'
+
+import bugImageUrl from './../assets/bug.png'
+import ideaImageUrl from './../assets/idea.png'
+import thoughtImageUrl from './../assets/thought.png'
+
+const feedbackTypes = {
+	BUG: {
+		title: 'Problema',
+		image: {
+			source: bugImageUrl,
+			alt: 'Imagem de um insecto',
+		}
+	},
+	IDEA: {
+		title: 'Ideia',
+		image: {
+			source: ideaImageUrl,
+			alt: 'Imagem de uma lâmpada',
+		}
+	},
+	OTHER: {
+		title: 'Outros',
+		image: {
+			source: thoughtImageUrl,
+			alt: 'Imagem de um balão de pensamento'
+		}
+	}
+}
+
 
 export function WidgetForm() {
 
@@ -5,9 +35,24 @@ export function WidgetForm() {
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 			<header>
 				<span className="text-xl leading-6">Deixe seu feedback</span>
+
+				<CloseButton />
 			</header>
 
-			<p>Hello, world!</p>
+			<div className="flex py-8 gap-2  w-full">
+				{ Object.entries(feedbackTypes).map(([key, value]) => {
+					console.log(key, ':', value)
+					return (
+						<button
+							className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+						>
+							<img src={value.image.source} alt={value.image.alt}/>
+							<span>{value.title}</span>
+						</button>
+						
+					)
+				}) }
+			</div>
 
 			<footer className="text-xs text-neutral-400">
 				Feito com (: pela <a className="underline underline-offset-2">Rocketseat</a>
