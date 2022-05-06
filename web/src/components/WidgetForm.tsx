9672c2eb3bf6abcee7e7f63d72bd93c217c698aa
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-06 02:35:41 +0200

    x as Type

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -30,11 +30,11 @@ const feedbackTypes = {
 	}
 }
 
-type FeedbackType = keyof typeof feedbackTypes
+type FeedbackType = keyof typeof feedbackTypes 
 
 
 export function WidgetForm() {
-	const [feedbackType, setFeedbackType] = useState(null)
+	const [feedbackType, setFeedbackType] = useState<FeedbackType || null>(null)
 
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -50,7 +50,7 @@ export function WidgetForm() {
 					return (
 						<button
 							key={key}
-							onCick={() => setFeedbackType(key)}
+							onCick={() => setFeedbackType(key as FeedbackType)}
 							className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
 
 						>
