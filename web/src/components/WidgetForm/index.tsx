bbee5a1619b3f039036675e1e42c9dfe98c5fb37
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-05-06 03:00:12 +0200

    refactoring

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 
-import { CloseButton } from './CloseButton'
+import { CloseButton } from '../CloseButton'
 
-import bugImageUrl from './../assets/bug.png'
-import ideaImageUrl from './../assets/idea.png'
-import thoughtImageUrl from './../assets/thought.png'
+import bugImageUrl from './../../assets/bug.png'
+import ideaImageUrl from './../../assets/idea.png'
+import thoughtImageUrl from './../../assets/thought.png'
 
 const feedbackTypes = {
 	BUG: {
@@ -30,11 +30,12 @@ const feedbackTypes = {
 	}
 }
 
-type FeedbackType = keyof typeof feedbackTypes 
+type FeedbackType = keyof typeof feedbackTypes;
+
 
 
 export function WidgetForm() {
-	const [feedbackType, setFeedbackType] = useState<FeedbackType || null>(null)
+	const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -45,26 +46,26 @@ export function WidgetForm() {
 			</header>
 
 			<div className="flex py-8 gap-2  w-full">
-				{ Object.entries(feedbackTypes).map(([key, value]) => {
+				{Object.entries(feedbackTypes).map(([key, value]) => {
 					console.log(key, ':', value)
 					return (
 						<button
 							key={key}
-							onCick={() => setFeedbackType(key as FeedbackType)}
+							onClick={() => setFeedbackType(key as FeedbackType)}
 							className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
 
 						>
-							<img src={value.image.source} alt={value.image.alt}/>
+							<img src={value.image.source} alt={value.image.alt} />
 							<span>{value.title}</span>
 						</button>
-						
+
 					)
-				}) }
+				})}
 			</div>
 
 			<footer className="text-xs text-neutral-400">
 				Feito com (: pela <a className="underline underline-offset-2">Rocketseat</a>
 			</footer>
-		</div> 
+		</div>
 	)
 }
