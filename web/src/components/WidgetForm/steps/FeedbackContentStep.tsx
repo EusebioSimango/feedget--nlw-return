736824689e1e99c34a77e08c625393151f5d63d3
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-06 19:20:25 +0200

    adding back button

--- web/src/components/WidgetForm/steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/steps/FeedbackContentStep.tsx
@@ -1,3 +1,4 @@
+import { ArrowLeft } from "phosphor-react"
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 
@@ -11,6 +12,10 @@ export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) 
   return (
     <>
       <header>
+        <button className="absolute left-5 top-5 text-zinc-400 hover:text-zinc-100">
+          <ArrowLeft />
+        </button>
+
         <span className="text-xl leading-6 flex gap-2 items-center">
           <img className="w-6 h-6 leading-6" src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} />
           {feedbackTypeInfo.title}
