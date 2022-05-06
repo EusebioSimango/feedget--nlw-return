4edb6967b08e6fa5228435c111ee39e41aeba708
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-06 13:03:44 +0200

    up

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -64,7 +64,7 @@ export function WidgetForm() {
 			</div>
 
 			<footer className="text-xs text-neutral-400">
-				Feito com (: pela <a className="underline underline-offset-2">Rocketseat</a>
+				Feito com (: pela <a className="underline underline-offset-2" href="/">Rocketseat</a>
 			</footer>
 		</div>
 	)
