3ef5564a1e50aef373178ace8596e48c6bb96974
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-08 01:33:14 +0200

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,19 @@ const PORT = 3333
 
 const app = express()
 
+app.use(express.json())
+
+app.post('feedbacks', (req, res) => {
+	console.log(req.body)
+
+	return res.send('Hello, World!')
+})
+
 app.get('/users', (req, res) => {
 	return res.send('Hello, World!')
 })
+
+
 
 app.listen(PORT, () => {
 	console.log("Server running at 'http://localhost:" + PORT + "'")
