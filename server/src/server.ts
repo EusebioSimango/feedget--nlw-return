f506e91f313ab1667ac6ca3db29cd4d2102581dd
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-08 00:37:10 +0200

    sending response res.send('x')

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,10 @@ const PORT = 3333
 
 const app = express()
 
+app.get('/users', (req, res) => {
+	return res.send('Hello, World!')
+})
+
 app.listen(PORT, () => {
 	console.log("Server running at 'http://localhost:" + PORT + "'")
 })
