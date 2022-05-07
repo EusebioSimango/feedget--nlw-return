7235dee294831816a291cd384050dfb6fb743c6f
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-07 22:15:54 +0200

    taking screenshot w/ html2canvas and passing it to base64 then send the img to parrentComponent n creating loading while screenshot its taking screenshot

--- web/src/components/WidgetForm/steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/steps/FeedbackContentStep.tsx
@@ -7,6 +7,7 @@ interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
   onFeedbackRestartRequested: () => void;
 }
+const [screenshot, setScreenshot] = useState<string | null>(null)
 
 
 export function FeedbackContentStep({ 
@@ -39,7 +40,7 @@ export function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <ScreenshotButton />
+          <ScreenshotButton onScreenshotTook={setScreenshot}/>
           <button 
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
