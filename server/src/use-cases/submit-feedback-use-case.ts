1a9f80071f1946463f3e68e567917a4fc9779a8e
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-17 21:59:26 +0200

    cleaning

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -17,9 +17,13 @@ export class SubmitFeedbackUseCase {
 	async execute(request: SubmitFeedbackUseCaseRequest) {
 		const { type, comment, screenshot } = request
 
-		if (!type) throw new Error('type is required.')
+		if (!type) {
+			throw new Error('type is required.')
+		}
 
-		if (!comment) throw new Error('comment is required.')
+		if (!comment) {
+			throw new Error('comment is required.')
+		}
 
 		if (screenshot && !screenshot.startsWith('data:image/png:base64')) {
 			throw new Error('Invalid screenshot format.')
